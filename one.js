8efbc38cc688e54d16db8d4105be1786330997b1
Szymon Sobczak <szymon.sobczak@gmail.com>
2013-09-14 09:51:43 +0200

    cert

--- one.js
+++ one.js
@@ -2,9 +2,26 @@ var https = require('https');
 
 api_key = "034dfb38-c110-4545-8ef1-9453dc83d4a9"
 
-https.get("https://192.168.151.182:8000/api3/?session=" +
-	api_key + "&command=getparty", function(res) {
-  console.log("Got response: " + res.statusCode);
+var req = https.request({ 
+      host: '192.168.151.182', 
+      port: 8000,
+      path: "/api3/?session=" + api_key + "&command=getparty",
+      method: 'GET',
+      rejectUnauthorized: false,
+      requestCert: true,
+      agent: false
+    }, function(res) {
+
+	var body = [];
+    res.on('data', function(data){
+        body.push(data);
+    });
+
+    res.on('end', function(){
+        console.log( body.join('') );
+    });
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
+
+req.end()
